b3b8b256bfa82fffed58a17c0d34ad2fe883964a
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-19 20:02:42 +0000

    Added ńew int variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace VariousTypes
+{
+
+
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int age = 21;
+            uint range =  4294967295U;
+            long negative = -99999930192;
+            ulong positive = 1999394850128492304; 
+
+            
+
+            
+            
+            Console.WriteLine("Hello LP1! \n" + age);
+            Console.WriteLine(range);
+            Console.WriteLine(negative);
+            Console.WriteLine(positive);
+
+        }
+    }
+}
