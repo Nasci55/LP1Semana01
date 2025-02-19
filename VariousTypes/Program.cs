b449073287f62e3273cf6a6dfcc98ce717eb3de0
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-19 21:05:10 +0000

    new real variables added

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -12,16 +12,23 @@ namespace VariousTypes
         private static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+            //INT variables
             int age = 21;
             uint range =  4294967295U;
             long negative = -99999930192;
             ulong positive = 1999394850128492304; 
 
-
+            //Chars variables
             char recycle = '\u267B';
             char infinite = '\u221E';
             char exclamation = '\u203C';
             
+            //Real Variables
+            double pi = 3.14159265359;
+            float shortPi = 3.1415f;
+            decimal  exactPi = 3.141592m;   
+
+
             
             Console.WriteLine("Hello LP1! \n" + age);
             Console.WriteLine(range);
@@ -30,6 +37,9 @@ namespace VariousTypes
             Console.WriteLine(recycle);
             Console.WriteLine(infinite);
             Console.WriteLine(exclamation);
+            Console.WriteLine(pi);
+            Console.WriteLine(shortPi);
+            Console.WriteLine(exactPi);
 
         }
     }
