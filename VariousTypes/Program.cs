dad2356bc127a76543db19a2c5470e72a21c2884
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-19 20:20:39 +0000

    new chars added

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace VariousTypes
 {
@@ -10,19 +11,25 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             int age = 21;
             uint range =  4294967295U;
             long negative = -99999930192;
             ulong positive = 1999394850128492304; 
 
-            
 
+            char recycle = '\u267B';
+            char infinite = '\u221E';
+            char exclamation = '\u203C';
             
             
             Console.WriteLine("Hello LP1! \n" + age);
             Console.WriteLine(range);
             Console.WriteLine(negative);
             Console.WriteLine(positive);
+            Console.WriteLine(recycle);
+            Console.WriteLine(infinite);
+            Console.WriteLine(exclamation);
 
         }
     }
