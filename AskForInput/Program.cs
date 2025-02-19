8c504bddff849c7a05c282b13a5581be3611b46d
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-02-19 22:25:04 +0000

    Asked for user input and stored it

---
+++ AskForInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine ("Please insert an integer number: ");
+            string userInput = Console.ReadLine();
+
+
+        }
+    }
+}
